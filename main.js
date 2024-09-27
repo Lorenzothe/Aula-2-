5e67b4de7fece1a00bee9521c385b514bc836cd2
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-09-27 09:40:24 -0300

    a

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function areaQuadrado(lado1){
     return lado1*lado1;
 }
 console.log(areaQuadrado(7));
+let areaQuadrado = parseInt (prompt ('qual a área?'))
+document.write(medias);
 
 /*Função Anônima*/
 
@@ -15,5 +17,3 @@ let media = function (me1, me2, me3) {
     return (me1+me2+me3)/3;
 }
 console.log(media(1,3,26));
-let media = parseInt (prompt ('qual sua nota?'))
-document.write(nota);
